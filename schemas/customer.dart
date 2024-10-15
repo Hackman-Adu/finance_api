ff84bf381d2509ba3c18764a99607f3ad8ab743c
Hackman Adu Gyamfi <hackmangyamfi31@gmail.com>
2024-10-15 06:53:33 +0000

    update customer schema

--- schemas/customer.dart
+++ schemas/customer.dart
@@ -3,9 +3,7 @@ import 'package:zod_validation/zod_validation.dart';
 import '../prisma/generated_dart_client/model.dart';
 
 Map<String, dynamic> createCustomerSchema = {
-  "last_name": Zod()
-      .type<String>('last_name should be a string')
-      .min(1, "Last name is requred"),
+  "last_name": Zod().min(1, "Last name is requred"),
   "location": Zod().min(1, "Location is required"),
   "other_names": Zod().min(1, "Other names is required"),
   "mobile": Zod().phone("Invalid mobile number"),
@@ -13,7 +11,11 @@ Map<String, dynamic> createCustomerSchema = {
 };
 
 Map<String, dynamic> updateCustomerSchema = {
-  "customer_id": Zod().min(1, "Customer id is required")
+  "customer_id": Zod().min(1, "Customer id is required"),
+  "last_name": Zod().optional().min(1, "Last name is requred"),
+  "location": Zod().optional().min(1, "Location is required"),
+  "other_names": Zod().optional().min(1, "Other names is required"),
+  "place_of_work": Zod().optional().min(1, "Place of work is required")
 };
 
 Map<String, dynamic> addPaymentMethodSchema = {
